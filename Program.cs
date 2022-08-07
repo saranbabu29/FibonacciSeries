c3973f5c8f24ba8298fedce2bd16a3936bcbf96b
saranbabu29 <sharanraj982@gmail.com>
2022-08-07 12:33:21 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3;
+            int i;
+            int number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
